2ac8c36d8b3e70ab6cc424c6669d8647c5a61b03
Nicole Stevens <nicci@ducksfeet.com>
2024-06-27 11:13:11 -0700

    Fixed names

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# sview - The Linux Systemd Unit Viewer
+
+## Description
+This program loads a list of systemd services and displays them with check boxes for "Active" and "Running". Toggling these checkboxes will trigger systemd commands to enable or disable the unit, or start or stop the unit 
+
+## Installation
+The script, setup.sh, must be run as root. It will install the program, policy file, polkit helper and the desktop menu file. 
+
+## Bugs 
+probably a lot. 
+
+## Documentation
+This is it, sadly.
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+we=$(basename $0)
+function abort() {
+	echo $@ >&1
+	echo "Aborting."
+	exit 1
+}
+policy_path="/usr/share/polkit-1/actions"
+helper_path="/usr/share/polkit-1/helpers"
+policy="com.ducksfeet.sview.policy"
+helper="com.ducksfeet.sview.helper"
+desktop="sview.desktop"
+binloc="/usr/bin"
+cat << EOF
+This script installs sview - the Linux Systemd Unit Viewer.
+Locations:
+Policy: ${policy_path}/${policy}
+Helper: ${helper_path}/${helper}
+Program: ${binloc}
+EOF
+
+if [[ "$UID" != "0" ]] ; then
+	echo "$we: error: this script must be run as root" >&1
+	exit 1
+fi
+install --backup -m 644 ${policy} ${policy_path}/${policy} || abort "Error installing policy"
+install --backup -D -m 755 ${helper} ${helper_path}/${helper} || abort "Error installing helper"
+install --backup -m 755 sview ${binloc}/sview || abort "Error installing program"
+xdg-desktop-menu install --novendor ${desktop}
+
