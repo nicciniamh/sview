f5da2db8e85ca48e66d1074eeecc991af02c7027
Nicole Stevens <nicci@ducksfeet.com>
2024-06-27 12:19:55 -0700

    Added checks for required modules

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ function abort() {
 	echo "Aborting."
 	exit 1
 }
+modules="gi pystemd"
 policy_path="/usr/share/polkit-1/actions"
 helper_path="/usr/share/polkit-1/helpers"
 policy="com.ducksfeet.sview.policy"
@@ -23,8 +24,12 @@ if [[ "$UID" != "0" ]] ; then
 	echo "$we: error: this script must be run as root" >&1
 	exit 1
 fi
+echo "Checking for required modules: ${modules}"
+for module in $modules ; do
+	./testmod ${module} || abort "Cannot import ${module} - this is required for installation"
+done
+echo "Modules are ok, installing files"
 install --backup -m 644 ${policy} ${policy_path}/${policy} || abort "Error installing policy"
 install --backup -D -m 755 ${helper} ${helper_path}/${helper} || abort "Error installing helper"
 install --backup -m 755 sview ${binloc}/sview || abort "Error installing program"
 xdg-desktop-menu install --novendor ${desktop}
-
